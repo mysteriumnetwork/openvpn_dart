4102e8adf76a1c52874ecc699156724dab852596
Kristijan Mitrikjeski <kmitrikjeski@gmail.com>
2025-12-03 16:02:53 +0100

    Refactor OpenVPN plugin for improved C API and code style
    Replaces the C API header with a minimal extern "C" registration function, updates installer reference from DLL to EXE in CMake, and refactors all plugin source files for consistent code style, namespace usage, and improved separation of C++/C API. Updates the test to check plugin status instead of platform version. These changes improve maintainability and compatibility with Flutter's plugin system.

--- windows/include/openvpn_dart/openvpn_dart_plugin_c_api.h
+++ windows/include/openvpn_dart/openvpn_dart_plugin_c_api.h
@@ -1,89 +1,24 @@
-#ifndef FLUTTER_PLUGIN_OPENVPN_DART_PLUGIN_H_
-#define FLUTTER_PLUGIN_OPENVPN_DART_PLUGIN_H_
+#ifndef FLUTTER_PLUGIN_OPENVPN_DART_PLUGIN_C_API_H_
+#define FLUTTER_PLUGIN_OPENVPN_DART_PLUGIN_C_API_H_
 
-#include <flutter/method_channel.h>
-#include <flutter/plugin_registrar_windows.h>
-#include <flutter/event_channel.h>
-#include <flutter/event_stream_handler_functions.h>
+#include <flutter_plugin_registrar.h>
 
-#include <memory>
-#include <string>
-#include <thread>
-#include <atomic>
-#include <mutex>
+#ifdef FLUTTER_PLUGIN_IMPL
+#define FLUTTER_PLUGIN_EXPORT __declspec(dllexport)
+#else
+#define FLUTTER_PLUGIN_EXPORT __declspec(dllimport)
+#endif
 
-namespace openvpn_dart {
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
 
-class OpenVpnDartPlugin : public flutter::Plugin {
- public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+    FLUTTER_PLUGIN_EXPORT void OpenvpnDartPluginCApiRegisterWithRegistrar(
+        FlutterDesktopPluginRegistrarRef registrar);
 
-  OpenVpnDartPlugin(flutter::PluginRegistrarWindows *registrar);
+#if defined(__cplusplus)
+} // extern "C"
+#endif
 
-  virtual ~OpenVpnDartPlugin();
-
-  // Disallow copy and assign.
-  OpenVpnDartPlugin(const OpenVpnDartPlugin&) = delete;
-  OpenVpnDartPlugin& operator=(const OpenVpnDartPlugin&) = delete;
-
- private:
-  // Called when a method is called on the plugin channel from Dart.
-  void HandleMethodCall(
-      const flutter::MethodCall<flutter::EncodableValue> &method_call,
-      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-
-  // OpenVPN process management
-  void StartVPN(const std::string& config);
-  void StopVPN();
-  void MonitorVPNStatus();
-  std::string GetCurrentStatus();
-  bool IsVPNRunning();
-
-  // TAP driver management
-  bool IsTAPDriverInstalled();
-  bool InstallTAPDriver();
-  std::string GetTAPAdapterName();
-
-  // Bundled OpenVPN setup
-  bool ExtractBundledOpenVPN();
-  std::string GetBundledOpenVPNPath();
-  std::string GetPluginDataPath();
-
-  // Event channel handlers
-  std::unique_ptr<flutter::StreamHandlerError<flutter::EncodableValue>> 
-      OnListenInternal(
-          const flutter::EncodableValue* arguments,
-          std::unique_ptr<flutter::EventSink<flutter::EncodableValue>>&& events);
-  
-  std::unique_ptr<flutter::StreamHandlerError<flutter::EncodableValue>> 
-      OnCancelInternal(const flutter::EncodableValue* arguments);
-
-  // Plugin registrar
-  flutter::PluginRegistrarWindows *registrar_;
-  
-  // Event sink for status updates
-  std::unique_ptr<flutter::EventSink<flutter::EncodableValue>> event_sink_;
-  std::mutex event_sink_mutex_;
-
-  // OpenVPN process handle
-  PROCESS_INFORMATION process_info_;
-  HANDLE process_handle_;
-  HANDLE pipe_read_;
-  HANDLE pipe_write_;
-  std::atomic<bool> is_connected_;
-  std::atomic<bool> is_monitoring_;
-  std::thread monitor_thread_;
-  std::thread log_thread_;
-  std::string current_status_;
-  std::mutex status_mutex_;
-  
-  // Paths
-  std::string config_file_path_;
-  std::string openvpn_executable_path_;
-  std::string bundled_path_;
-  std::string log_file_path_;
-};
-
-}  // namespace openvpn_dart
-
-#endif  // FLUTTER_PLUGIN_OPENVPN_DART_PLUGIN_H_
+#endif // FLUTTER_PLUGIN_OPENVPN_DART_PLUGIN_C_API_H_
--- windows/openvpn_dart_plugin.h
+++ windows/openvpn_dart_plugin.h
@@ -12,78 +12,80 @@
 #include <atomic>
 #include <mutex>
 
-namespace openvpn_dart {
+namespace openvpn_dart
+{
 
-class OpenVpnDartPlugin : public flutter::Plugin {
- public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+    class OpenVpnDartPlugin : public flutter::Plugin
+    {
+    public:
+        static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
 
-  OpenVpnDartPlugin(flutter::PluginRegistrarWindows *registrar);
+        OpenVpnDartPlugin(flutter::PluginRegistrarWindows *registrar);
 
-  virtual ~OpenVpnDartPlugin();
+        virtual ~OpenVpnDartPlugin();
 
-  // Disallow copy and assign.
-  OpenVpnDartPlugin(const OpenVpnDartPlugin&) = delete;
-  OpenVpnDartPlugin& operator=(const OpenVpnDartPlugin&) = delete;
+        // Disallow copy and assign.
+        OpenVpnDartPlugin(const OpenVpnDartPlugin &) = delete;
+        OpenVpnDartPlugin &operator=(const OpenVpnDartPlugin &) = delete;
 
- private:
-  // Called when a method is called on the plugin channel from Dart.
-  void HandleMethodCall(
-      const flutter::MethodCall<flutter::EncodableValue> &method_call,
-      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+        // Called when a method is called on the plugin channel from Dart.
+        void HandleMethodCall(
+            const flutter::MethodCall<flutter::EncodableValue> &method_call,
+            std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
 
-  // OpenVPN process management
-  void StartVPN(const std::string& config);
-  void StopVPN();
-  void MonitorVPNStatus();
-  std::string GetCurrentStatus();
-  bool IsVPNRunning();
+    private:
+        // OpenVPN process management
+        void StartVPN(const std::string &config);
+        void StopVPN();
+        void MonitorVPNStatus();
+        std::string GetCurrentStatus();
+        bool IsVPNRunning();
 
-  // TAP driver management
-  bool IsTAPDriverInstalled();
-  bool InstallTAPDriver();
-  std::string GetTAPAdapterName();
+        // TAP driver management
+        bool IsTAPDriverInstalled();
+        bool InstallTAPDriver();
+        std::string GetTAPAdapterName();
 
-  // Bundled OpenVPN setup
-  bool ExtractBundledOpenVPN();
-  std::string GetBundledOpenVPNPath();
-  std::string GetPluginDataPath();
+        // Bundled OpenVPN setup
+        bool ExtractBundledOpenVPN();
+        std::string GetBundledOpenVPNPath();
+        std::string GetPluginDataPath();
 
-  // Event channel handlers
-  std::unique_ptr<flutter::StreamHandlerError<flutter::EncodableValue>> 
-      OnListenInternal(
-          const flutter::EncodableValue* arguments,
-          std::unique_ptr<flutter::EventSink<flutter::EncodableValue>>&& events);
-  
-  std::unique_ptr<flutter::StreamHandlerError<flutter::EncodableValue>> 
-      OnCancelInternal(const flutter::EncodableValue* arguments);
+        // Event channel handlers
+        std::unique_ptr<flutter::StreamHandlerError<flutter::EncodableValue>>
+        OnListenInternal(
+            const flutter::EncodableValue *arguments,
+            std::unique_ptr<flutter::EventSink<flutter::EncodableValue>> &&events);
 
-  // Plugin registrar
-  flutter::PluginRegistrarWindows *registrar_;
-  
-  // Event sink for status updates
-  std::unique_ptr<flutter::EventSink<flutter::EncodableValue>> event_sink_;
-  std::mutex event_sink_mutex_;
+        std::unique_ptr<flutter::StreamHandlerError<flutter::EncodableValue>>
+        OnCancelInternal(const flutter::EncodableValue *arguments);
 
-  // OpenVPN process handle
-  PROCESS_INFORMATION process_info_;
-  HANDLE process_handle_;
-  HANDLE pipe_read_;
-  HANDLE pipe_write_;
-  std::atomic<bool> is_connected_;
-  std::atomic<bool> is_monitoring_;
-  std::thread monitor_thread_;
-  std::thread log_thread_;
-  std::string current_status_;
-  std::mutex status_mutex_;
-  
-  // Paths
-  std::string config_file_path_;
-  std::string openvpn_executable_path_;
-  std::string bundled_path_;
-  std::string log_file_path_;
-};
+        // Plugin registrar
+        flutter::PluginRegistrarWindows *registrar_;
 
-}  // namespace openvpn_dart
+        // Event sink for status updates
+        std::unique_ptr<flutter::EventSink<flutter::EncodableValue>> event_sink_;
+        std::mutex event_sink_mutex_;
 
-#endif  // FLUTTER_PLUGIN_OPENVPN_DART_PLUGIN_H_
+        // OpenVPN process handle
+        PROCESS_INFORMATION process_info_;
+        HANDLE process_handle_;
+        HANDLE pipe_read_;
+        HANDLE pipe_write_;
+        std::atomic<bool> is_connected_;
+        std::atomic<bool> is_monitoring_;
+        std::thread monitor_thread_;
+        std::thread log_thread_;
+        std::string current_status_;
+        std::mutex status_mutex_;
+
+        // Paths
+        std::string config_file_path_;
+        std::string openvpn_executable_path_;
+        std::string bundled_path_;
+        std::string log_file_path_;
+    };
+
+} // namespace openvpn_dart
+
+#endif // FLUTTER_PLUGIN_OPENVPN_DART_PLUGIN_H_
